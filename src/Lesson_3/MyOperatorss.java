50731d77a3e19c9e8b117c1f2efab8da9c2d2209
Maksimus <maxwww76@gmail.com>
2025-06-05 00:03:10 +0300

    m

---
+++ src/Lesson_3/MyOperatorss.java
@@ -0,0 +1,16 @@
+package Lesson_3;
+
+public class MyOperatorss {
+    // IDE (Project) - GIT (Virtual project)
+    // Document ADDED Добавление документа
+// Commit Точка сохранения
+// Changes applied Изменения внесены - отражены
+
+// $ git add .
+// $ git commit -am "SOME MESSAGE"
+
+// $ git push подгрузить все изменения на ГитХаб
+
+// $ git fetch - проверить количество коммитов на ГитХабе и сравнить с количеством коммитов на локальном Гите
+// $ git status - получить результат сравнения
+}
