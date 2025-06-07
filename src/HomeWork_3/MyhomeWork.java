671e18db828edc18611d7cccac52abd722286420
Maksimus <maxwww76@gmail.com>
2025-06-07 17:23:27 +0300

    hommework

---
+++ src/HomeWork_3/MyhomeWork.java
@@ -0,0 +1,77 @@
+package HomeWork_3;
+
+import java.util.Scanner;
+
+public class MyhomeWork {
+    public static void main(String[] args){
+        getSwitc();
+    }
+    // Написать программу для вывода поры года по номеру месяца
+    // использовать оператор switch/case
+//    Времена года:
+//    Winter: (зима)
+//    Spring: (весна)
+//    Summer: (лето)
+//    Autumn: (осень, в британском английском)
+//    Fall: (осень, в американском английском)
+//    Месяцы:
+//    January: (январь)
+//    February: (февраль)
+//    March: (март)
+//    April: (апрель)
+//    May: (май)
+//    June: (июнь)
+//    July: (июль)
+//    August: (август)
+//    September: (сентябрь)
+//    October: (октябрь)
+//    November: (ноябрь)
+//    December: (декабрь
+    public static void getSwitc(){
+        System.out.println("Plese enter your number (1 to 12)");
+        Scanner input = new Scanner(System.in);
+        int mouth = input.nextInt();
+        switch (mouth) {
+            case 12:
+                System.out.println(" Your Season is Winter");
+                break;
+            case 1:
+                System.out.println(" Your Season is Winter");
+                break;
+            case 2:
+                System.out.println(" Your Season is Winter");
+                break;
+            case 3:
+                System.out.println(" Your Season is Spring");
+                break;
+            case 4:
+                System.out.println(" Your Season is Spring");
+                break;
+            case 5:
+                System.out.println(" Your Season is Spring");
+                break;
+            case 6:
+                System.out.println(" Your Season is Summer");
+                break;
+            case 7:
+                System.out.println(" Your Season is Summer");
+                break;
+            case 8:
+                System.out.println(" Your Season is Summer");
+                break;
+            case 9:
+                System.out.println(" Your Season is Fall");
+                break;
+            case 10:
+                System.out.println(" Your Season is Fall");
+                break;
+            case 11:
+                System.out.println(" Your Season is Fall");
+                break;
+        }
+
+
+
+    }
+
+}
