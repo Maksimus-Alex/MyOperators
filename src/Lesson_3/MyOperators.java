b62564164796788c2eedc7be28f3c94b64788402
Maksimus <maxwww76@gmail.com>
2025-06-07 12:10:33 +0300

    Chang 1

--- src/Lesson_3/MyOperators.java
+++ src/Lesson_3/MyOperators.java
@@ -1,4 +1,27 @@
 package Lesson_3;
 
+// $ git add .
+// $ git commit -am "SOME MESSAGE"
+
+// $ git push подгрузить все изменения на ГитХаб
+
+// $ git fetch - проверить количество коммитов на ГитХабе и сравнить с количеством коммитов на локальном Гите
+// $ git status - получить результат сравнения
+
+// Lesson 3 -> Operators
+// if, else if, switch - flow operators
+// while, do while, for - loops (циклы)
+// Три ключевых слова: break, continue, return
+
+
+import java.io.IOException;
+
 public class MyOperators {
+    public static void main(String[] args) throws IOException {
+        System.out.print("Pleas enter a value: ");
+
+
+        int value = System.in.read();
+        System.out.println("You have entered the fjlloving symbl: " + (char) value);
+    }
 }
