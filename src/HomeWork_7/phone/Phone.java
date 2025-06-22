13d03ad63f9d2e5ae5637880aad5eb7a6207a940
Maksimus <maxwww76@gmail.com>
2025-06-22 23:29:43 +0300

    Phone

---
+++ src/HomeWork_7/phone/Phone.java
@@ -0,0 +1,13 @@
+package HomeWork_7.phone;
+
+public class Phone {
+    private long number;
+    private String model;
+    private long weight;
+
+    public Phone( long number, String model,long weight){
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+}
