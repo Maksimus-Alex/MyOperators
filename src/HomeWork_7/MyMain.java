77b0aabca91f879eed90ae8b3cbca21c5eac535a
Maksimus <maxwww76@gmail.com>
2025-06-23 23:45:03 +0300

    iphone

--- src/HomeWork_7/MyMain.java
+++ src/HomeWork_7/MyMain.java
@@ -1,4 +1,21 @@
 package HomeWork_7;
 
+import HomeWork_7.phone.Phone;
+
 public class MyMain {
+
+    public static void main(String[] args) {
+        getIformationsPhone();
+    }
+    public static void getIformationsPhone(){
+        Phone phone = new Phone(80295853262L, "Sumsung", 209 );
+        System.out.println(phone.getIformationPhone());
+
+        System.out.println("**************");
+
+        Phone iphone = new Phone(375442256892L, "IPHON11", 255);
+        System.out.println(iphone.getIformationPhone());
+
+
+    }
 }
--- src/HomeWork_7/phone/Phone.java
+++ src/HomeWork_7/phone/Phone.java
@@ -3,11 +3,21 @@ package HomeWork_7.phone;
 public class Phone {
     private long number;
     private String model;
-    private long weight;
+    private double weight;
 
-    public Phone( long number, String model,long weight){
+    public Phone(long number, String model, double weight) {
         this.number = number;
         this.model = model;
         this.weight = weight;
     }
+
+    public  String getIformationPhone() {
+        return "Your Iformation Phone is: " + "\n" +
+                "Phone number: " + this.number + "\n" +
+                "Phone model: " + this.model + "\n" +
+                "Phone weight: " + this.weight;
+
+    }
+
+
 }
