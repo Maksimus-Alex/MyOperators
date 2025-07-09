81d8383aa6178d91bbe99fdff6a0f93830df254d
Maksimus <maxwww76@gmail.com>
2025-07-10 00:11:19 +0300

    10.0

--- src/HomeWork_10/ProcesDocument/ProcessDocument.java
+++ src/HomeWork_10/ProcesDocument/ProcessDocument.java
@@ -2,19 +2,19 @@ package HomeWork_10.ProcesDocument;
 
 public class ProcessDocument {
 
-        public static void functionalDocument(String documentNumber) {
-            // 1 - вывести на экран строку два первых блока по 4 цифры
-            String[] blocks = documentNumber.split("-");
-            System.out.println("First two blocks of numbers: "
-                    + blocks[0] + "-" + blocks[2] + "\n");
-        }
+    public static void functionalDocument(String documentNumber) {
+        // 1 - вывести на экран строку два первых блока по 4 цифры
+        String[] blocks = documentNumber.split("-");
+        System.out.println("First two blocks of numbers: "
+                + blocks[0] + "-" + blocks[2] + "\n");
+    }
 
-            public static void replaceAll(String documentNumber) {
-                // 2 - вывести на экран номер документа , нг блоки из 3 букв
-                // заменить на ***
-                String stars = documentNumber.replaceAll("[a-z]{3}", "***");
-                System.out.println("Replacing: " + stars);
-            }
+    public static void replaceAll(String documentNumber) {
+        // 2 - вывести на экран номер документа , нг блоки из 3 букв
+        // заменить на ***
+        String stars = documentNumber.replaceAll("[a-z]{3}", "***");
+        System.out.println("Replacing: " + stars);
+    }
 
     public static void numDocument(String documentNumber) {
         // 3 - Вывести на экран только одни буквы из номера документа
@@ -31,33 +31,33 @@ public class ProcessDocument {
     }
 
 
-        public static void checkDocument(String documentNumber) {
+    public static void checkDocument(String documentNumber) {
 
-            // 5 - Проверить содержит ли номер документа последовательность abc
-            if (documentNumber.toLowerCase().contains("abc")) {
-                System.out.println("Found 'abc'");
-            } else {
-                System.out.println("Not found 'abc'");
-            }
-
-            // 6 - Проверить начинается ли номер документа с последовательности 555
-            if (documentNumber.startsWith("555")) {
-                System.out.println("Start with: 555");
-            } else {
-                System.out.println("Doesn't start 555");
-            }
-
-            // 7 - Проверить заканчивается ли номер документа на последовательность 1а2b
-            if (documentNumber.toLowerCase().endsWith("1a2b")) {
-                System.out.println("Ends with 1а2b");
-            } else {
-                System.out.println("Doesn't end with 1а2b");
-            }
+        // 5 - Проверить содержит ли номер документа последовательность abc
+        if (documentNumber.toLowerCase().contains("abc")) {
+            System.out.println("Found 'abc'");
+        } else {
+            System.out.println("Not found 'abc'");
         }
 
+        // 6 - Проверить начинается ли номер документа с последовательности 555
+        if (documentNumber.startsWith("555")) {
+            System.out.println("Start with: 555");
+        } else {
+            System.out.println("Doesn't start 555");
+        }
 
-       // Вывести буквы в формате "Letters: yyy/yyy/y/y" в верхнем регистре
-    public static void originalDocument(String documentNumber){
+        // 7 - Проверить заканчивается ли номер документа на последовательность 1а2b
+        if (documentNumber.toLowerCase().endsWith("1a2b")) {
+            System.out.println("Ends with 1а2b");
+        } else {
+            System.out.println("Doesn't end with 1а2b");
+        }
+    }
+
+
+    //  4 - Вывести буквы в формате "Letters: yyy/yyy/y/y" в верхнем регистре
+    public static void originalDocument(String documentNumber) {
         String latters = documentNumber;
         StringBuilder latter = new StringBuilder();
         for (int i = 0; i < latters.length(); i++) {
@@ -71,8 +71,7 @@ public class ProcessDocument {
     }
 
 
-
-    }
+}
 
 
 
